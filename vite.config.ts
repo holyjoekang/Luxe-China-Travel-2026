3fcf843b1e7f9d7a05f09ae014a373b3e1d9ee34
holyjoekang <146902251+holyjoekang@users.noreply.github.com>
2026-07-31 11:20:56 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+
+    base: '/Luxe-China-Travel-2026/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
